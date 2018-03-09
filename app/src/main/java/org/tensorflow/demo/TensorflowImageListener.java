ea19c03f84f3700a44f0fb79cdeb4b4bc6082e4a
yangchihyuan <yangchihyuan@gmail.com>
2018-03-09 14:39:39 +0800

    Sakura is ready to report the TinyYOLO results, so that I want to backup the old code running TensorFlow on Zenbo.

--- app/src/main/java/org/tensorflow/demo/TensorflowImageListener.java
+++ app/src/main/java/org/tensorflow/demo/TensorflowImageListener.java
@@ -24,6 +24,7 @@ import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.graphics.RectF;
 import android.media.Image;
 import android.media.Image.Plane;
@@ -37,11 +38,32 @@ import junit.framework.Assert;
 import org.tensorflow.demo.env.ImageUtils;
 import org.tensorflow.demo.env.Logger;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.sql.Timestamp;
 import java.util.List;
+import java.lang.Math;
 
 import com.asus.robotframework.API.RobotAPI;
 import com.asus.robotframework.API.RobotCallback;
 
+import java.net.HttpURLConnection;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * Class that takes in preview frames and converts the image to Bitmaps to process with Tensorflow.
@@ -78,14 +100,15 @@ class TensorFlowImageListener implements OnImageAvailableListener {
   private byte[][] yuvBytes;
   private int[] rgbBytes = null;
   private Bitmap rgbFrameBitmap = null;
-  private Bitmap croppedBitmap = null;
+  private Bitmap shrink_for_fullwidth_Bitmap = null;
 
   private boolean computing = false;
-  private boolean readyForNextImage = true;
+  //private boolean readyForNextImage = true;
   private Handler handler;
 
   private RecognitionScoreView scoreView;
   private BoundingBoxView boundingView;
+  private InputView inputView;
 
   private com.asus.robotframework.API.RobotAPI YOLO_robotAPI;
   private YOLO_RobotCallback robotCallback;
@@ -94,6 +117,7 @@ class TensorFlowImageListener implements OnImageAvailableListener {
       final AssetManager assetManager,
       final RecognitionScoreView scoreView,
       final BoundingBoxView boundingView,
+      final InputView inputView,
       final Handler handler,
       final Integer sensorOrientation) {
     Assert.assertNotNull(sensorOrientation);
@@ -102,6 +126,7 @@ class TensorFlowImageListener implements OnImageAvailableListener {
         INPUT_NAME, OUTPUT_NAME);
     this.scoreView = scoreView;
     this.boundingView = boundingView;
+    this.inputView = inputView;
     this.handler = handler;
     this.sensorOrientation = sensorOrientation;
     this.robotCallback = new YOLO_RobotCallback();
@@ -139,6 +164,9 @@ class TensorFlowImageListener implements OnImageAvailableListener {
     //LOGGER.i("sensorOrientationImageListener" + sensorOrientation.toString());
 
     final Canvas canvas = new Canvas(dst);
+    Paint fgPaint = new Paint();
+    fgPaint.setColor(0xff00ff00);
+    canvas.drawRect(0,0,448,448,fgPaint);
     canvas.drawBitmap(src, matrix, null);
   }
 
@@ -153,14 +181,12 @@ class TensorFlowImageListener implements OnImageAvailableListener {
         return;
       }
 
-//      if (computing || !readyForNextImage || !robotCallback.motion_complete) {
-      if (computing || !readyForNextImage ) {
+//      if (computing || !robotCallback.motion_complete) {
+      if (computing ) {
         image.close();
         return;
       }
 
-//      robotCallback.motion_complete = false;
-      readyForNextImage = true;
       computing = true;
 
       Trace.beginSection("imageAvailable");
@@ -175,7 +201,6 @@ class TensorFlowImageListener implements OnImageAvailableListener {
         LOGGER.i("Initializing at size %dx%d", previewWidth, previewHeight);
         rgbBytes = new int[previewWidth * previewHeight];
         rgbFrameBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Config.ARGB_8888);
-        croppedBitmap = Bitmap.createBitmap(INPUT_SIZE, INPUT_SIZE, Config.ARGB_8888);
 
         yuvBytes = new byte[planes.length][];
         for (int i = 0; i < planes.length; ++i) {
@@ -213,53 +238,178 @@ class TensorFlowImageListener implements OnImageAvailableListener {
     }
 
     rgbFrameBitmap.setPixels(rgbBytes, 0, previewWidth, 0, 0, previewWidth, previewHeight);
-    drawResizedBitmap(rgbFrameBitmap, croppedBitmap);
+    shrink_for_fullwidth_Bitmap = Bitmap.createBitmap(INPUT_SIZE, INPUT_SIZE, Config.ARGB_8888);
+    drawResizedBitmap(rgbFrameBitmap, shrink_for_fullwidth_Bitmap);
+    inputView.setBitmap(shrink_for_fullwidth_Bitmap);
 
-//    handler.post(
-//        new Runnable() {
-//          @Override
-//          public void run() {
-            final List<Classifier.Recognition> results = tensorflow.recognizeImage(croppedBitmap);
+/*      URL url = null;
+      try {
+          url = new URL("http://140.112.30.188:8889");
+      } catch (MalformedURLException e) {
+          e.printStackTrace();
+      }
+      HttpURLConnection connection = null;
+      try {
+          connection = (HttpURLConnection) url.openConnection();
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+      */
+      try {
 
-            LOGGER.v("%d results", results.size());
-            for (final Classifier.Recognition result : results) {
-              LOGGER.v("Result: " + result.getTitle());
+          String UPLOAD_URL = "http://140.112.30.188:8890";
+
+          String username = "test_user_123";
+          String datetime = "2016-12-09 10:00:00";
+//          File file_image = new File("/sdcard/DCIM/Camera/IMG_20180305_144519.jpg");
+//          int file_size = Integer.parseInt(String.valueOf(file_image.length()));
+          final okhttp3.OkHttpClient.Builder httpBuilder = new OkHttpClient.Builder();
+          OkHttpClient client  = httpBuilder
+                  .connectTimeout(1, TimeUnit.SECONDS)
+                  .writeTimeout(1, TimeUnit.SECONDS)
+                  .build();
+
+          int bytes = shrink_for_fullwidth_Bitmap.getByteCount();
+          ByteBuffer buffer_ARGB = ByteBuffer.allocate(bytes); //Create a new buffer
+          shrink_for_fullwidth_Bitmap.copyPixelsToBuffer(buffer_ARGB); //Move the byte data to the buffer
+          byte[] array_ARGB = buffer_ARGB.array();
+          //The format is RGBA
+          //3/8/2018 Chih-Yuan: Convert the RGBA format to RGB. I don't know how to do it.
+
+// Create a multipart request body. Add metadata and files as 'data parts'.
+          RequestBody requestBody = new MultipartBody.Builder()
+                  .setType(MultipartBody.FORM)      //should we use form? According to the okhttp3 API, there is a MIXED type.
+//                  .addFormDataPart("username", username)
+//                  .addFormDataPart("datetime", datetime)
+                  //.addFormDataPart("file","FileTestName")    //the String FileTestName will show as the data field.
+                  //.addFormDataPart("image", "TestImageName",
+                  //        RequestBody.create(MediaType.parse("image/jpeg"), array))
+                  .addFormDataPart("application/octet-stream", "Framedata", RequestBody.create(MediaType.parse("application/octet-stream"), array_ARGB))
+                  .build();
+
+// Create a POST request to send the data to UPLOAD_URL
+          Request request = new Request.Builder()
+                  .url(UPLOAD_URL)
+                  .post(requestBody)
+                  .build();
+
+// Execute the request and get the response from the server
+          Response response = null;
+
+          try {
+              response = client.newCall(request).execute();
+              //3/8/2018 Check here, what can I get?
+          } catch (IOException e) {
+              e.printStackTrace();
+          }
+
+// Check the response to see if the upload succeeded
+          if (response == null || !response.isSuccessful()) {
+              //Log.w("Example", "Unable to upload to server.");
+          } else {
+              //System.out.println(response.body().string());
+              LOGGER.d("%s", response.body().string());
+          }
+      } catch (Exception e) {
+          e.printStackTrace();
+      } finally {
+          //connection.disconnect();
+      }
+
+
+/*
+          String twoHyphens = "--";
+          String boundary =  "*****"+Long.toString(System.currentTimeMillis())+"*****";
+          String lineEnd = "\r\n";
+
+          connection.setDoOutput(true);
+          connection.setDoInput(true);
+          connection.setUseCaches(false);
+          connection.setRequestMethod("POST");
+          connection.setRequestProperty("Connection", "Keep-Alive");
+          connection.setRequestProperty("User-Agent", "Android Multipart HTTP Client 1.0");
+          connection.setRequestProperty("Content-Type", "multipart/form-data; boundary="+boundary);
+
+          //connection.setFixedLengthStreamingMode(802816);
+          connection.setChunkedStreamingMode(0);  //use this when the output size is unknown
+
+        DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+          outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+          //the name will be the key in the server side, the filename keeps the same, the content_type is image/jpeg
+          outputStream.writeBytes("Content-Disposition: form-data; name=\"" + "test123" + "\"; filename=\"" + "test456" +"\"" + lineEnd);
+          outputStream.writeBytes("Content-Type: image/jpeg" + lineEnd);
+          outputStream.writeBytes("Content-Transfer-Encoding: binary" + lineEnd);
+          outputStream.writeBytes(lineEnd);
+        int bytes = shrink_for_fullwidth_Bitmap.getByteCount();
+        ByteBuffer buffer = ByteBuffer.allocate(bytes); //Create a new buffer
+        shrink_for_fullwidth_Bitmap.copyPixelsToBuffer(buffer); //Move the byte data to the buffer
+
+        byte[] array = buffer.array();
+          outputStream.write(array, 0, bytes);
+          outputStream.writeBytes(lineEnd);
+        //  out.writeBytes("Test 123");
+          outputStream.flush();
+          outputStream.close();
+
+//        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+//        readStream(in);
+    } catch (IOException e) {
+          e.printStackTrace();
+      } finally {
+        connection.disconnect();
+    }
+*/
+
+
+    //2/23/2018 Chih-Yuan: This handle in fact is the inferenceHandler declared in the CameraConnectionFragment.java
+/*        final boolean post = handler.post(
+            new Runnable() {
+                @Override
+                public void run() {
+                    final List<Classifier.Recognition> results = tensorflow.recognizeImage(shrink_for_fullwidth_Bitmap);
+                    computing = false;
+                    scoreView.setResults(results);
+                    boundingView.setResults(shrink_for_fullwidth_Bitmap,results);
+
+                    float Confidence = results.get(0).getConfidence();
+                    float Confidence_threshold = 0.13f;
+                    if (Confidence > Confidence_threshold) {
+                        RectF preBoundingBox = results.get(0).getLocation();
+                        float boundingbox_center_x = preBoundingBox.left;   //in fact, the meaning of the .left property is the box center
+                        float boundingbox_half_width = preBoundingBox.right;
+                        float boundingbox_center_y = preBoundingBox.top;      //2/12/2018 Chih-Yuan: I will this value later to adjust Zenbo's neck angle
+                        float boundingbox_half_height = preBoundingBox.bottom;
+                        float boundingbox_area = 2 * boundingbox_half_width * 2 * boundingbox_half_height;
+                        float expected_boundingbox_area = (INPUT_SIZE / 2) * (INPUT_SIZE / 2);
+
+                        float x = (float) expected_boundingbox_area / boundingbox_area - 1;      //the unit is meter
+                        //Zenbo's horizotal view angle: 62.5053
+                        float rotate_rate = 0.9f;
+                        float full_rotation_angle = (float) -(boundingbox_center_x - INPUT_SIZE / 2) / INPUT_SIZE * (62.5053f / 180) * 3.14f;
+                        float y = x * (float) Math.tan(full_rotation_angle);
+
+                        float theta = full_rotation_angle * rotate_rate;
+                        LOGGER.d("center_x %f theta %f", boundingbox_center_x, theta);
+                        //              if (SAVE_PREVIEW_BITMAP) {
+                        //                ImageUtils.saveBitmap(shrink_for_fullwidth_Bitmap);
+                        //              }
+                        //2/22/2018 Chih-Yuan: break the previous motion action
+                        int result = YOLO_robotAPI.motion.stopMoving();
+                        robotCallback.motion_complete = false;
+                        YOLO_robotAPI.motion.moveBody(x, y, theta);
+                    }
+
+                }
             }
-            scoreView.setResults(results);
-            boundingView.setResults(results);
-
-            if( results.get(0).getTitle().equals("person") ){
-              RectF preBoundingBox = results.get(0).getLocation();
-              float center_x = preBoundingBox.left;   //in fact, the meaning of the .left property is the box center
-
-              float x = (float) 0;
-              float y = (float) 0;
-              //Zenbo's horizotal view angle: 62.5053
-              float rotate_rate = 0.9f;
-              float theta = (float) -(center_x - INPUT_SIZE/2 )/INPUT_SIZE * (62.5053f/180) * 3.14f * rotate_rate;
-              LOGGER.d("center_x %f theta %f", center_x, theta);
-              if (SAVE_PREVIEW_BITMAP) {
-                ImageUtils.saveBitmap(croppedBitmap);
-              }
-
-//              if( results.get(0).getConfidence() > 0.3f)
-//                robotCallback.motion_complete = false;
-                YOLO_robotAPI.motion.moveBody(x,y, theta);
-            }
-//            else
-//            {
-//              robotCallback.motion_complete = true;
-//            }
-
-            computing = false;
-//          }
-//        });
-
-    Trace.endSection();
+        );
+*/
+    computing = false;
+    robotCallback.motion_complete = false;
+      Trace.endSection();
   }
 
-  public void takePic() {
-    readyForNextImage = true;
+  public void takePic() {   //1/18/2017 Chih-Yuan: this function is never called.
+    //readyForNextImage = true;
     LOGGER.v("Taking picture");
     return;
   }
